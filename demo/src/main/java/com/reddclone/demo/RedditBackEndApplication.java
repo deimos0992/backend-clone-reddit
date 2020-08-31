9264209d1dc6e4427a1efee61c7662c205775571
deimos <luigi.benvenuto01@gmail.com>
2020-08-31 12:31:23 +0200

    first commit

---
+++ demo/src/main/java/com/reddclone/demo/RedditBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.reddclone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditBackEndApplication.class, args);
+	}
+
+}
